88e43375bbed4d7a7425fa20122293d15a7d1b37
Julian <julian.kmazo@gmail.com>
2015-03-16 17:51:32 -0500

    Models company and areas (company and category) updated

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,2 +1,3 @@
 from base_model import BaseModel
 from recover_account_token import RecoverAccountToken
+from company import Company
---
+++ core/models/company.py
@@ -0,0 +1,10 @@
+from core.models import BaseModel
+from google.appengine.ext import ndb
+
+
+class Company(BaseModel):
+    name = ndb.StringProperty()
+    domain = ndb.StringProperty()
+    urldomain = ndb.StringProperty()
+    areas = ndb.KeyProperty(repeated=True)
+    logo = ndb.StringProperty()
